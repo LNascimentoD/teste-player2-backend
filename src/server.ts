52403f16ed129694ca80c38e4bd9c0d617ed4da0
Lucas do Nascimento Diniz <lucaszf.ln@gmail.com>
2021-08-11 20:09:00 -0300

    Config project and architecture completed

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.json({"nome": "lucas do "})
+})
+
+app.listen(3333);
